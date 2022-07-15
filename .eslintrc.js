a605c48365f36d685c873fce470d52b0a09bab76
Logan McAnsh <logan@mcan.sh>
2022-07-15 17:08:23 -0400

    chore: deploy via vercel
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/internal",
+  ],
+};
--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,9 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: [".*"]
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
+  serverBuildTarget: "vercel",
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  ignoredRouteFiles: ["**/.*"],
 };
