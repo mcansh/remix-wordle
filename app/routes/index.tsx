02472f7ab9c10e68f2c2d26e685c822b866fe7b6
Logan McAnsh <logan@mcan.sh>
2022-07-18 11:00:46 -0400

    chore: button doesnt support enterKeyHint
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -408,15 +408,13 @@ export default function IndexPage() {
             );
           })}
 
-          <button
+          <input
             form="current-guess"
-            enterKeyHint="enter"
+            enterKeyHint="send"
             type="submit"
-            disabled={"winner" in data || data.currentGuess === TOTAL_GUESSES}
             className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-          >
-            Submit Guess
-          </button>
+            value="Submit Guess"
+          />
         </div>
       </main>
     </div>
