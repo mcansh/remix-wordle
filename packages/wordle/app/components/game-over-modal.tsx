b6529c371b70e228a4e3262e752dcac8490f6eb9
Logan McAnsh <logan@mcan.sh>
2026-01-13 18:08:51 +0000

    fix(GameOverModal): update background opacity
    Signed-off-by: GitHub <noreply@github.com>

--- packages/wordle/app/components/game-over-modal.tsx
+++ packages/wordle/app/components/game-over-modal.tsx
@@ -30,7 +30,7 @@ export function GameOverModal(
   return () => {
     return (
       <div className="relative z-10" role="dialog" aria-modal="true">
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        <div className="fixed inset-0 bg-gray-500/75 transition-opacity" />
         <div className="fixed inset-0 z-10 overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
