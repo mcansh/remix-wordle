e3c27df3a77e20eaca1a1c12f41ed7dac415f4c0
Logan McAnsh <logan@mcan.sh>
2026-04-04 00:12:55 -0400

    feat: add conditional skip for security headers based on request method
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    (cherry picked from commit 777b58412d494f2e3074428d301ab43ff850674b)

--- app/router.ts
+++ app/router.ts
@@ -39,6 +39,9 @@ middleware.push(session(sessionCookie, sessionStorage))
 middleware.push(asyncContext())
 middleware.push(
 	securityHeaders({
+		skip(context) {
+			return context.method !== "GET"
+		},
 		"Content-Security-Policy": {
 			"default-src": [NONE],
 			"script-src": [SELF],
